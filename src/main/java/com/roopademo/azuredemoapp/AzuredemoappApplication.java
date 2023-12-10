68b7ec761df4abf3c865c29e1c946c36b8ed3b75
roopa <roopa.bhavik@gmail.com>
2023-12-10 14:48:34 -0500

    first commit

---
+++ src/main/java/com/roopademo/azuredemoapp/AzuredemoappApplication.java
@@ -0,0 +1,20 @@
+package com.roopademo.azuredemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzuredemoappApplication {
+	
+    @GetMapping("/om")
+	public String getData() {
+		return "OM SAMARTHA SADGURU SAINATH";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(AzuredemoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roopademo/azuredemoapp/AzuredemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roopademo.azuredemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
